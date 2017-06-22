daab3b83fa79162eec24c49ace83a7e1218cd48e
Ivy Patton <lilaivy@gmail.com>
2017-06-21 19:49:34 -0700

    changed resource to friends

--- server.js
+++ server.js
@@ -1,29 +1,52 @@
 
 const Hapi = require('hapi');
+const mongojs = require('mongojs');
 
+//creating server with host/port here:
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('Hello, world!');
-  }
+//connecting to Mongo here:
+server.app.db = mongojs('hapi-rest-mongo', ['friends']);
+
+//Load plugins and start server
+server.register([
+    require('./routes/friends')
+], (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    // Start the server
+    server.start((err) => {
+        console.log('Server running at:', server.info.uri);
+    });
+
 });
 
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function (request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
+
+//routes:
+// server.route({
+//     method: 'GET',
+//     path: '/',
+//     handler: function (request, reply) {
+//         reply('Hello, world!');
+//     }
+// });
+
+// server.route({
+//     method: 'GET',
+//     path: '/friends',
+//     handler: function (request, reply) {
+//         reply('friends will show here soon!');
+//     }
+// });
 
 server.start((err) => {
 
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at: ${server.info.uri}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
 });
