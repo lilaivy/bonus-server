9b41184cb251de2afb405282eec2f8b57c8814e9
Ivy Patton <lilaivy@gmail.com>
2017-06-21 19:11:17 -0700

    hello world rendering at local host

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
 });
 
-//add resource
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
+server.start((err) => {
+
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
